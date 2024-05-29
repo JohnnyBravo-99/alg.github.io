d3dba90ca43f5c8f5de3efd46b330d228f7a986d
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-05-29 02:24:05 -0400

    Tailwind, Html edit to index.html using tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./*.html'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+
+    extend: {},
+  },
+  plugins: [],
+}
+
