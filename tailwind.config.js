0026e5ed238b168ef24d93ba83babf5b657fcea2
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-05-29 04:22:07 -0400

    added content src

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,10 @@ module.exports = {
   purge: ['/*.html'],
   darkMode: false, // or 'media' or 'class'
   content: [
-  './*.html',           // Your HTML files in the root directory
-  './src/**/*.html',    // Any HTML files in the src directory
-  './src/**/*.js',      // Any JavaScript files in the src directory
-  './src/**/*.jsx',     // Any JSX files in the src directory
-  './src/**/*.ts',      // Any TypeScript files in the src directory
-  './src/**/*.tsx',     // Any TSX files in the src directory
-  // Add other file types or directories as needed
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Include your source files
   ],
+  
   theme: {
     extend: {},
   },
