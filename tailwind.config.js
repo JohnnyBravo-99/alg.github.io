fe0ffcbc288ac600dfe524cb7ed1af531edb5fec
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-05-29 05:43:06 -0400

    startover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['/*.html'],
-  content: [
-    '.alg.github.io/public/**/*.html',
-    '.alg.github.io/src/**/*.{css,js,jsx,ts,tsx}', // Include your source files
-  ],
-  
+  content: [],
   theme: {
-    extend: {},
-  },
-  variants: {
-
     extend: {},
   },
   plugins: [],
