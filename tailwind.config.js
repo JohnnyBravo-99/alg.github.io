b463638aaa4c938fbdf66ad34eb6c21033e3b3dc
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-05-29 03:54:31 -0400

    package.json, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./*.html'],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
-  content: [],
+  content: [
+  './*.html',           // Your HTML files in the root directory
+  './src/**/*.html',    // Any HTML files in the src directory
+  './src/**/*.js',      // Any JavaScript files in the src directory
+  './src/**/*.jsx',     // Any JSX files in the src directory
+  './src/**/*.ts',      // Any TypeScript files in the src directory
+  './src/**/*.tsx',     // Any TSX files in the src directory
+  // Add other file types or directories as needed
+  ],
   theme: {
     extend: {},
   },
