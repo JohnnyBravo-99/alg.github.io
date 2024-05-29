8abcb53d181590cacf9f57d6bf946fe5d6b8313f
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-05-29 05:01:46 -0400

    npm run build:css
    Needed to edit some of the tailwind config files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['/*.html'],
-  darkMode: false, // or 'media' or 'class'
   content: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx}', // Include your source files
+    '.alg.github.io/public/**/*.html',
+    '.alg.github.io/src/**/*.{css,js,jsx,ts,tsx}', // Include your source files
   ],
   
   theme: {
